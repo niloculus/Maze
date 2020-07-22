50f53c947ffadf870260d8e83610a0a7fda9e8a4
niloculus <avu@muhlenberg.edu>
2020-07-22 18:27:56 -0400

    Added test 2

--- testMaze2020.py
+++ testMaze2020.py
@@ -6,7 +6,8 @@ class MazeTests(unittest.TestCase):
     def test_maze(self):
         m = Maze()
         self.assertTrue(type(m.screen) == turtle._Screen,"No Screen! type is" + str(type(m.screen)))
-
+    def test_2(self):
+        pass
 
 if __name__=="__main__":
     unittest.main()

f32ab4275fd31ef20916a090af4aac028c58b9f9
niloculus <avu@muhlenberg.edu>
2020-07-23 18:45:25 -0400

    Tested for Turtle

--- Maze.py
+++ Maze.py
@@ -4,3 +4,6 @@ import turtle
 class Maze:
     def __init__(self):
         self.screen = turtle.getscreen()
+    def get_turtle(self):
+        self.t=turtle.turtles()[0]
+        return(self.t)
--- testMaze2020.py
+++ testMaze2020.py
@@ -6,8 +6,11 @@ class MazeTests(unittest.TestCase):
     def test_maze(self):
         m = Maze()
         self.assertTrue(type(m.screen) == turtle._Screen,"No Screen! type is" + str(type(m.screen)))
-    def test_2(self):
-        pass
+    def test_turtle(self):
+        m = Maze()
+        t = m.get_turtle()
+        self.assertTrue(type(t)==turtle.Turtle, f"returned {type(t)}")
+
 
 if __name__=="__main__":
     unittest.main()

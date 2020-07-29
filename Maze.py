1bc9c6dcf7a75b01821d46b34c00365707682f72
niloculus <avu@muhlenberg.edu>
2020-07-29 16:45:09 -0400

    Screen is 400x400

--- Maze.py
+++ Maze.py
@@ -1,14 +1,27 @@
 import turtle
-
-
+SIZE = 400
 class Maze:
     def __init__(self):
         self.screen = turtle.getscreen()
+
     def get_turtle(self):
-        self.t=turtle.turtles()[0]
-        return(self.t)
+        self.t = turtle.turtles()[0]
+        return (self.t)
     def bgcolor(self):
         self.b = turtle.bgcolor('blue')
+    def screensize(self):
+        self.s = turtle.getscreen()
+        self.screen.setup(400, 400)
+        # self.screen.screensize(400,400)
+
+        # self.screen.screensize(400, 400)
+        # self.s = turtle.screensize(400, 400)
+
+
+        # self.s.screensize(400, 400)
+
+
+
 
 
 
--- testMaze2020.py
+++ testMaze2020.py
@@ -1,5 +1,6 @@
 import turtle
 import unittest
+SIZE = 400
 from Maze import Maze
 
 class MazeTests(unittest.TestCase):
@@ -13,6 +14,19 @@ class MazeTests(unittest.TestCase):
     def test_background(self):
         b = self.m.bgcolor()
         self.assertTrue(self.m.screen.bgcolor() == "blue" , f"the color is {self.m.screen.bgcolor()}")
+    def test_size(self):
+        s = self.m.screensize()
+        self.assertTrue(self.m.screen.window_width() == SIZE)
+        self.assertTrue(self.m.screen.window_height() == SIZE)
+
+        # self.assertTrue(s[0] == SIZE, f"the x-dim is {s[0]}")
+        # self.assertTrue(s[1] == SIZE, f"the y-dim is {s[1]}")
+
+        # s = self.m.screensize()
+        # self.assertTrue(self.m.screensize() == "SIZE , SIZE" , f"the size is {self.m.screensize()}")
+
+
+
 
 
 

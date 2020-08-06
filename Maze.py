483045c8ec301d3d6c79fc5e9a602012f37b2edd
niloculus <avu@muhlenberg.edu>
2020-08-06 17:59:56 -0400

    Maze 10 Done

--- Maze.py
+++ Maze.py
@@ -1,49 +1,52 @@
 import turtle
 SIZE = 400
+EAST=0
+NORTH=1
+WEST=2
+SOUTH=3
 class Maze:
     def __init__(self):
-        self.screen = turtle.getscreen()
-        turtle.setup(400, 400)
-        self.matrix = [[1 for i in range(20)] for j in range(20)]
-        # self.screen.screensize(400,400) works for 2nd and 3rd test_Size
+        self.reset()
     def get_turtle(self):
         self.t = turtle.turtles()[0]
         return (self.t)
-
-
     def reset(self):
         self.screen = turtle.getscreen()
         turtle.setup(400, 400)
         self.t = turtle.turtles()[0]
         self.t.penup()
-        self.t.goto(-(SIZE / 2 - 10), SIZE / 2 - 10)
+        self.t.goto(-(SIZE / 2 - 20), SIZE / 2 - 20)
         self.matrix = [[1 for i in range(int(SIZE / 20))] for i in range(int(SIZE / 20))]
-        self.b = turtle.bgcolor('blue')
+        self.screen.bgcolor('blue')
         self.t.shape('square')
         self.t.color('white')
         self.t.stamp()
         self.matrix[0][0] = 0
+    def getMatrixValueAt(self, pos):
+        m = Maze()
+        m.reset()
+        x = int((pos[0] + 180) / 20)
+        y = 20 - int((pos[1] + 200) / 20) - 1
+        v = self.matrix[x][y]
+        return v
+    def setMatrixValueAt(self, pos, value):
+        x = int((pos[0] + 200) / 20)
+        y = 20 - int((pos[1] + 200) / 20) - 1
+        try:
+            self.matrix[y][x] = value
+        except:
+            return False
+        if value == 0:
+            self.t.color('white')
+            self.t.stamp()
+        if value == 1:
+            self.t.color('blue')
+            self.t.stamp()
+            return True
 
-    # def getMatrixValueAt(self, pos):
-    #     m.reset()
-    #     value = m.getMatrixValueAt(m.turtle.position)
-    #     m.setMatrixValueAt(m.turtle.position, value)
-    #     x = int((pos[0] + 200) / 20)
-    #     y = 20 - int((pos[1] + 200) / 20) - 1
-    #     # WRITE CODE TO ASSIGN THE MATRIX VALUE AT x,y TO v
-    #     return v
 
 
-
-
-        # self.screen.screensize(400, 400)
-        # self.s = turtle.screensize(400, 400)
-
-
-        # self.s.screensize(400, 400)
-
-
-
-
+# def dig(self, d):
+    #     return self.t.pos()
 
 
--- testMaze2020.py
+++ testMaze2020.py
@@ -1,6 +1,7 @@
 import turtle
 import unittest
 SIZE = 400
+
 from Maze import Maze
 
 class MazeTests(unittest.TestCase):
@@ -16,22 +17,25 @@ class MazeTests(unittest.TestCase):
     def test_size(self):
         self.assertTrue(self.m.screen.window_width() == SIZE)
         self.assertTrue(self.m.screen.window_height() == SIZE)
-
-        # s = self.m.screen.screensize() second line of code
-        # self.assertTrue(s[0] == SIZE, f"the x-dim is {s[0]}")
-        # self.assertTrue(s[1] == SIZE, f"the y-dim is {s[1]}")
-
-        # self.assertTrue(self.m.screen.screensize() == (SIZE , SIZE) , f"the size is {self.m.screen.screensize()}")
     def testMatrixSize(self):
         self.assertTrue(len(self.m.matrix)==SIZE/20)
     def testReset(self):
         self.m.reset()
         self.assertTrue(self.m.matrix[0][0] == 0)
         p = self.m.t.pos()
-        self.assertTrue(self.m.t.pos() == (-(SIZE / 2 - 10), SIZE / 2 - 10))
+        self.assertTrue(self.m.t.pos() == (-(SIZE / 2 - 20), SIZE / 2 - 20))
+    def testSettingMatrixValues(self):
+        m = Maze()
+        m.reset()
+        self.assertTrue(m.getMatrixValueAt(m.t.pos()) == 0)
+        self.assertTrue(m.setMatrixValueAt(m.t.pos(), 1))
+        self.assertTrue(self.m.matrix[0][0], 1)
 
-    # def Test_getMatrixValue(self):
-    #     self.assertTrue(m.getMatrixValueAt(m.turtle.position) == 0)
+    # def testdig(self):
+    #     m=Maze()
+    #     m.reset()
+    #     r = m.dig(EAST)
+    #     self.assertTrue(r ==(-160,180), "got " + str(r))
 
 if __name__=="__main__":
     unittest.main()
